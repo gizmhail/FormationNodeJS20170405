a4e32e6bdfeec5a24d079672d6f188232e6314d3
Sébastien Poivre <sebastien.poivre@orange.com>
2017-04-06 17:11:24 +0200

    Add write implemantion based on denoeified fs methods

--- FileContactService.js
+++ FileContactService.js
@@ -29,7 +29,7 @@ class FileContactservice {
   }
 
   write(contacts, callback) {
-    WriteImplems.callbacks(this.path, contacts, callback);
+    WriteImplems.promise(this.path, contacts, callback);
   }
 
   // Call also be used with a add(contactData, callback) signature
--- WriteImplems.js
+++ WriteImplems.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const denodeify = require('denodeify');
+
+const readFile = denodeify(fs.readFile);
+const writeFile = denodeify(fs.writeFile);
+const rename = denodeify(fs.rename);
 
 module.exports.initialCallbacks = function(path, contacts, callback) {
   let contactDescriptions = JSON.stringify(contacts);
@@ -22,4 +27,31 @@ module.exports.callbacks = function(path, contacts, callback) {
       });
     });
   });
+}
+
+module.exports.promise = function(path, contacts, callback){
+  let backupPath = path + ".back"
+  let contactDescriptions = JSON.stringify(contacts);
+  readFile(path)
+  .then( (previousData) => {
+    return writeFile(backupPath, previousData);
+  })
+  .then( () => {
+    return writeFile(path, contactDescriptions);
+  })
+  .then( () => {
+    callback(null);
+  })
+  .catch( (newWriteError) => {
+    console.log("Error triggered");
+    return rename(backupPath, path)
+    .then( () => {
+      console.log("Rename ok");
+      callback(newWriteError);
+    })
+    .catch( (renameError) => {
+      console.log("Rename error triggered");
+      callback(renameError)
+    });
+  })
 }
