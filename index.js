597e29e216bad183b3abfd2c18e2cbf72c940b86
Sébastien Poivre <sebastien.poivre@orange.com>
2017-04-06 10:59:08 +0200

    Clean package.json

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 #!/usr/local/bin/node
 
-// Depedancies
 const FileContactservice = require('./FileContactservice');
 const CLI = require('./CLI');
 
-// Internal components
 let contactService = new FileContactservice();
 CLI.activateForService(contactService);
