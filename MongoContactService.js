14d32922800d096d8213fc2744043d0e883a7e73
Sébastien Poivre <sebastien.poivre@orange.com>
2017-04-07 15:44:15 +0200

    Create a mongoDB based storage and associated service

---
+++ MongoContactService.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var contactSchema = new Schema({
+  id:  Number,
+  firstName: String,
+  lastName:   String
+});
+
+var ContactModel = mongoose.model('Contact', contactSchema);
+
+
+
+
+class MongoContactService {
+  constructor() {
+    mongoose.connect('mongodb://localhost/test');
+  }
+
+  read(callback) {
+    ContactModel.find({}, (error, contacts) => {
+      callback(error, contacts);
+    });
+  }
+
+  add(firstName, lastName, callback) {
+    let args = arguments;
+    ContactModel.findOne().sort('-id').exec(function(err, data){
+      let id = 0;
+      if(data){
+        id = data.id + 1;
+      }
+      let contactData = null;
+      if(args.length == 2) {
+        contactData = {};
+        for(var k in args[0]) {
+          if(['id', 'firstName', 'lastName'].indexOf(k) != -1) {
+            contactData[k] = args[0][k];
+          }
+        }
+        callback = args[1];
+        if(contactData.id == undefined) {
+          contactData.id = id;
+        }
+      } else {
+        contactData = {id: id, firstName: firstName, lastName: lastName};
+      }
+      let contact = new ContactModel(contactData);
+      contact.save( (error, contactObj) => {
+        callback(error, contactObj);
+      });
+
+    });
+  }
+
+  delete(contactId, callback) {
+    ContactModel.find({id: contactId}).remove(()=>{
+      callback(null, []);
+    });
+  }
+
+  watch(callback) {
+    //TODO
+  }
+}
+
+module.exports = MongoContactService;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/local/bin/node
 
 const FileContactservice = require('./FileContactservice');
+const MongoContactService = require('./MongoContactService');
 const CLI = require('./CLI');
 
-let contactService = new FileContactservice();
+// let contactService = new FileContactservice();
+let contactService = new MongoContactService();
 CLI.activateForService(contactService);
